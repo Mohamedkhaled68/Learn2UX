ae217917aec5bcb878be62656d577cff3f8b94dc
Mohamedkhaled68 <mohamed.khaled6083@gmail.com>
2025-11-13 20:29:38 +0200

    enhance 3

--- components/AyaContainer.tsx
+++ components/AyaContainer.tsx
@@ -2,7 +2,7 @@ const AyaContainer = () => {
     return (
         <p
             style={{ direction: "rtl" }}
-            className="text-[#363636] font-aya text-[16px] lg:text-[18px] 2xl:text-[28px]"
+            className="text-[#363636] font-aya text-[16px] lg:text-[18px] 2xl:text-[28px] text-center"
         >
             ﴿ ٱقْرَأْ بِٱسْمِ رَبِّكَ ٱلَّذِى خَلَقَ ۞ خَلَقَ ٱلْإِنسَٰنَ مِنْ
             عَلَقٍ ۞ ٱقْرَأْ وَرَبُّكَ ٱلْأَكْرَمُ ۞ ٱلَّذِى عَلَّمَ بِٱلْقَلَمِ
